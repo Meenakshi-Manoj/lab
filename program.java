922ff21d05912241de740e87af288e98e9202406
Meenakshi Manoj <73237796+Meenakshi-Manoj@users.noreply.github.com>
2021-02-10 10:47:06 +0530

    Add files via upload

---
+++ program.java
@@ -0,0 +1,18 @@
+class program
+{
+ public static void main(String[]args)
+{
+  int a,b,c;
+  try {
+     
+     a=8;
+     b=4;
+     c=a/b;
+  System.out.println(c);
+ }
+catch(Exception e)
+ {
+   System.out.println("division is not done");
+}
+}
+}
